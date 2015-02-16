a4f8449a2206b9fbd8336c8019d0639e6df0cb0e
Soheil Koushan <soheil.koushan@gmail.com>
2015-02-16 15:54:09 -0500

    Update profile fragment UI

--- app/src/main/java/com/blueoxfords/peacecorpstinder/fragments/ProfileFragment.java
+++ app/src/main/java/com/blueoxfords/peacecorpstinder/fragments/ProfileFragment.java
@@ -7,12 +7,14 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.blueoxfords.peacecorpstinder.Constants;
 import com.blueoxfords.peacecorpstinder.R;
 import com.blueoxfords.peacecorpstinder.activities.LoginActivity;
 import com.parse.ParseUser;
+import com.squareup.picasso.Picasso;
 
 /**
  * Created by soheil on 15-02-14.
@@ -24,9 +26,10 @@ public class ProfileFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View rootView = inflater.inflate(R.layout.fragment_profile, container, false);
 
+        ParseUser currentUser = ParseUser.getCurrentUser();
         TextView userName = (TextView) rootView.findViewById(R.id.userName);
-        userName.setText((String) ParseUser.getCurrentUser().get(Constants.ParseColumns.NICKNAME));
-
+        userName.setText((String) currentUser.get(Constants.ParseColumns.NICKNAME));
+        Picasso.with(getActivity()).load(currentUser.getString(Constants.ParseColumns.PICTURE_URL)).into((ImageView) rootView.findViewById(R.id.profile));
         Button signOutButton = (Button) rootView.findViewById(R.id.signOutButton);
         signOutButton.setOnClickListener(new View.OnClickListener() {
             @Override
